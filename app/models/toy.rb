b683156cf5a64d49efea7b863f12c4a35ae1059c
Christian M. Brandalise <christian.brandalise@gmail.com>
2014-12-13 00:42:17 +0000

    AWS setup

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -5,7 +5,7 @@ class Toy < ActiveRecord::Base
     validates :category, presence: true
     
     #Used for image upload | thoughtbot/paperclip gem. 
-    has_attached_file :image
+    has_attached_file :image, :bucket  => ENV['brinquedotecaphotos']
     
     #image upload validations.
     validates_attachment :image, :less_than => 5.megabytes, presence: true, 
